d943960d1a04cda6c356dd6f9321d38309fe7cfd
Cody Windows 7 <clsa@DEAN1-LAPTOP>
2021-08-16 12:21:02 -0400

    Created a UI layout with connected slots for the buttons

--- src/BPTruBPM200/BPTruBPM200.cpp
+++ src/BPTruBPM200/BPTruBPM200.cpp
@@ -4,4 +4,35 @@ BPTruBPM200::BPTruBPM200(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    SetupSlots();
+}
+
+void BPTruBPM200::SetupSlots()
+{
+    connect(ui.StartButton, SIGNAL(clicked()), this, SLOT(OnStartClicked()));
+    connect(ui.StopButton, SIGNAL(clicked()), this, SLOT(OnStopClicked()));
+    connect(ui.ClearButton, SIGNAL(clicked()), this, SLOT(OnClearClicked()));
+    connect(ui.ReviewButton, SIGNAL(clicked()), this, SLOT(OnReviewClicked()));
+    connect(ui.CycleButton, SIGNAL(clicked()), this, SLOT(OnCycleClicked()));
+}
+
+void BPTruBPM200::OnStartClicked() {
+    ui.ErrorsTextBrowser->setText("Start Pressed");
+}
+
+void BPTruBPM200::OnStopClicked() {
+    ui.ErrorsTextBrowser->setText("Stop Pressed");
+}
+
+void BPTruBPM200::OnClearClicked() {
+    ui.ErrorsTextBrowser->setText("Clear Pressed");
+}
+
+void BPTruBPM200::OnReviewClicked() {
+    ui.ErrorsTextBrowser->setText("Review Pressed");
+}
+
+void BPTruBPM200::OnCycleClicked() {
+    ui.ErrorsTextBrowser->setText("Cycle Pressed");
 }
--- src/BPTruBPM200/BPTruBPM200.h
+++ src/BPTruBPM200/BPTruBPM200.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 #include <QtWidgets/QMainWindow>
 #include "ui_BPTruBPM200.h"
 
@@ -9,7 +10,13 @@ class BPTruBPM200 : public QMainWindow
 
 public:
     BPTruBPM200(QWidget *parent = Q_NULLPTR);
-
+public slots:
+    void OnStartClicked();
+    void OnStopClicked();
+    void OnClearClicked();
+    void OnReviewClicked();
+    void OnCycleClicked();
 private:
     Ui::BPTruBPM200Class ui;
+    void SetupSlots();
 };
