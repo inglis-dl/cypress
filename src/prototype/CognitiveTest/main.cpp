b63448948233050200a2a891920bc88f545eeb5d
Cody Windows 7 <clsa@DEAN1-LAPTOP>
2021-11-16 11:09:10 -0500

    CommandLine Parse code copied and commented out. Need to merge changes from development to use

--- src/prototype/CognitiveTest/main.cpp
+++ src/prototype/CognitiveTest/main.cpp
@@ -1,5 +1,7 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include "../../auxiliary/CommandLineParser.h"
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +12,33 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
 
-    //TODO: Add commandLineParser from auxiliary
+    // Need to pull changes from development branch to get all features of commandLineParser
+    /*CommandLineParser parser;
+    QString errMessage;
+    switch (parser.parseCommandLine(app, &errMessage))
+    {
+    case CommandLineParser::CommandLineHelpRequested:
+        QMessageBox::warning(0, QGuiApplication::applicationDisplayName(),
+            "<html><head/><body><pre>"
+            + parser.helpText() + "</pre></body></html>");
+        return 0;
+    case  CommandLineParser::CommandLineVersionRequested:
+        QMessageBox::information(0, QGuiApplication::applicationDisplayName(),
+            QGuiApplication::applicationDisplayName() + ' '
+            + QCoreApplication::applicationVersion());
+        return 0;
+    case CommandLineParser::CommandLineOk:
+        break;
+    case CommandLineParser::CommandLineError:
+    case CommandLineParser::CommandLineInputFileError:
+    case CommandLineParser::CommandLineOutputPathError:
+    case CommandLineParser::CommandLineMissingArg:
+    case CommandLineParser::CommandLineModeError:
+        QMessageBox::warning(0, QGuiApplication::applicationDisplayName(),
+            "<html><head/><body><h2>" + errMessage + "</h2><pre>"
+            + parser.helpText() + "</pre></body></html>");
+        return 1;
+    }*/
 
     // Start App
     CognitiveTest w;
