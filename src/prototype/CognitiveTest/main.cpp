0428de9b1eef174ea6b8a79a07c131172b3f83c1
Cody Windows 7 <clsa@DEAN1-LAPTOP>
2021-11-16 13:00:23 -0500

    Added commandLineParser

--- src/prototype/CognitiveTest/main.cpp
+++ src/prototype/CognitiveTest/main.cpp
@@ -2,6 +2,7 @@
 #include <QtWidgets/QApplication>
 #include "../../auxiliary/CommandLineParser.h"
 #include <QMessageBox>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     // Need to pull changes from development branch to get all features of commandLineParser
-    /*CommandLineParser parser;
+    CommandLineParser parser;
     QString errMessage;
     switch (parser.parseCommandLine(app, &errMessage))
     {
@@ -38,14 +39,14 @@ int main(int argc, char *argv[])
             "<html><head/><body><h2>" + errMessage + "</h2><pre>"
             + parser.helpText() + "</pre></body></html>");
         return 1;
-    }*/
+    }
 
     // Start App
     CognitiveTest w;
-    w.setInputFileName("C:/Users/clsa/source/repos/CypressBuilds/CognitiveTest/CognitiveTestInput.json");
-    w.setOutputFileName("C:/Users/clsa/source/repos/CypressBuilds/CognitiveTest/Output");
-    w.setMode("live");
-    w.setVerbose(true);
+    w.setInputFileName(parser.getInputFilename());
+    w.setOutputFileName(parser.getOutputFilename());
+    w.setMode(parser.getMode());
+    w.setVerbose(parser.getVerbose());
 
     w.initialize();
     w.show();
