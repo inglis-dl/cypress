696fe23d8e58060ee9173ca3f4317816f5e6e3ea
inglis-dl <inglis.dl@gmail.com>
2022-01-20 13:48:27 -0500

    ENH: qtusb example - basic example showing/testing use of QtUsb lib

---
+++ src/examples/qtusb/MainWindow.cpp
@@ -0,0 +1,22 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    for(auto&& id : m_usb.devices())
+    {
+        QString line = "usb device id " + id;
+        ui->listWidget->addItem(line);
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
