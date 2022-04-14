cf7749c120451df0b9d3f171bb3f7d2e65881614
Cody Windows 7 <clsa@DEAN1-LAPTOP>
2022-04-14 16:13:12 -0400

    WIP: Adding ParadoxReader to git

---
+++ src/managers/ParadoxReader.cpp
@@ -0,0 +1,303 @@
+#include "ParadoxReader.h"
+
+ParadoxReader::ParadoxReader(QWidget* parent, const QString& filePath)
+{
+    // Open Database connection
+    openDatabase(filePath);
+}
+
+ParadoxReader::~ParadoxReader()
+{
+    closeDatabase();
+}
+
+/*
+* Reads in the data stored in a paradox database
+*/
+QJsonObject ParadoxReader::Read()
+{
+    // Create output json object
+    QJsonObject output;
+
+    // seek to start of database
+    m_dbFile.seek(0);
+
+    // Read the header data
+    bool errorReadingHeader = false;
+    try {
+        readHeader();
+    }
+    catch(...){
+        errorReadingHeader = true;
+    }
+    
+    // Report error if header could not be read or if header data is not valid
+    if (errorReadingHeader || false == headerDataValid()) {
+        handleError(&output, "Error: Valid header data could not be read");
+        return output;
+    }
+
+    // Read the actual data
+    QList<ParadoxDbBlock> blocks = readBlocks();
+
+    // Close Database connection
+    closeDatabase();
+    qDebug() << "Databases read in";
+
+    // Convert to Json Object
+    // Return error if there is no data found
+    if (blocks.isEmpty()) {
+        handleError(&output, "Error: No data stored in database blocks");
+        return output;
+    }
+
+    for (int i = 0; i < blocks.count(); i++) {
+        // TODO: add this to json output
+        blocks[i].readRecords(m_dbFile);
+    }
+    //QList<ParadoxRecord> records = blocks[0].readRecords(m_dbFile);
+
+    // return
+    output.insert("ErrorOccured", false);
+    return QJsonObject();
+}
+
+/*
+* Open connection to paradox database
+*/
+void ParadoxReader::openDatabase(const QString& filePath)
+{
+    // Open ZGripTest.DB
+    m_dbFile.setFileName(filePath);
+    m_dbFile.open(QFile::ReadOnly);
+}
+
+/*
+* Close connection with paradox database if it is currently open
+*/
+void ParadoxReader::closeDatabase()
+{
+    // Close ZGripTest.DB
+    if (m_dbFile.isOpen())
+    {
+        m_dbFile.close();
+    }
+}
+
+/*
+* Read in the header data from the paradox database
+*/
+void ParadoxReader::readHeader()
+{
+    m_header.recordSize = readShort(0x0000);
+    m_header.headerSize = readShort(0x0002);
+    m_header.fileType = readUByte(0x0004);
+    m_header.maxTableSize = readUByte(0x0005);
+    m_header.numRecords = readInt(0x0006);
+    m_header.nextBlock = readUShort(0x000A);
+    m_header.fileBlocks = readUShort(0x000C);
+    m_header.firstBlock = readUShort(0x000E);
+    m_header.lastBlock = readUShort(0x0010);
+    m_header.modifiedFlags1 = readUByte(0x0014);
+    m_header.indexFieldNumber = readUByte(0x0015);
+    m_header.primaryIndexWorkspace = readPtr(0x0016);
+    m_header.numFields = readShort(0x0021);
+    m_header.primaryKeyFields = readShort(0x0023);
+    m_header.encryption1 = readInt(0x0025);
+    m_header.sortOrder = readUByte(0x0029);
+    m_header.modifiedFlags2 = readUByte(0x002A);
+    m_header.changeCount1 = readUByte(0x002D);
+    m_header.changeCount2 = readUByte(0x002E);
+    m_header.tableNamePtr = readPtr(0x0030);
+    m_header.fieldInfoPtr = readPtr(0x0034);
+    m_header.writeProtected = readUByte(0x0038);
+    m_header.fileVersionId = readUByte(0x0039);
+    m_header.maxBlocks = readUShort(0x003A);
+    m_header.auxPassword = readUByte(0x003D);
+    m_header.cryptInfoStartPtr = readPtr(0x0040);
+    m_header.cryptInfoEndPtr = readPtr(0x0044);
+    m_header.autoInc = readInt(0x0049);
+    m_header.indexUpdateRequired = readUByte(0x004F);
+    m_header.refIntegrity = readUByte(0x0055);
+
+    seekToFieldInfoStart();
+    readHeaderFieldInfo();
+    skipPastUneededInfo();
+    readHeaderFieldNames();
+}
+
+bool ParadoxReader::headerDataValid()
+{
+    // TODO: fill this in
+    return true;
+}
+
+/*
+* Seek to the start of the header field info stored in the paradox database
+* Expects that the dbFile will be set to the end of the refIntegrity
+*/
+void ParadoxReader::seekToFieldInfoStart()
+{
+    // If header version is greater than 4...
+    if (m_header.fileVersionId > 0x04) {
+        // Seek to the start of the field info
+        m_dbFile.seek(0x0078);
+    }
+    // otherwise the field info should start after the refIntegrity
+}
+
+/*
+* Reads in the header field info stored in the paradox database
+* Expects the dbFile to be set to the start of the fieldInfo
+*/
+void ParadoxReader::readHeaderFieldInfo()
+{
+    for (int i = 0; i < m_header.numFields; i++) {
+        QByteArray bytes = m_dbFile.read(1);
+        quint8 type = (quint8)bytes[0];
+
+        bytes = m_dbFile.read(1);
+        quint8 size = (quint8)bytes[0];
+
+        qDebug() << i << ". " << "type: " << type << " size: " << size;
+        DbHeaderField headerField(static_cast<FieldType>(type), size);
+        m_header.fields.append(headerField);
+    }
+}
+
+/*
+* Skips past data that does not need to be read in yet
+* Expects that the dbFile will be set to the end of the field info
+*/
+void ParadoxReader::skipPastUneededInfo()
+{
+    // Skip tableNamePtr
+    m_dbFile.skip(4);
+
+    // skipFieldnamePtrArray
+    int numFields = (int)(m_header.numFields);
+    m_dbFile.skip(4 * numFields);
+
+    // Skip tableName
+    m_dbFile.skip(m_header.fileVersionId > 0x04 ? 261 : 79);
+}
+
+/*
+* Reads in the header field names stored in the paradox database
+* Expects the dbFile to be set to the start of the field names
+*/
+void ParadoxReader::readHeaderFieldNames()
+{
+    for (int i = 0; i < m_header.numFields; i++) {
+        QString fieldname;
+        quint8 next = (quint8)m_dbFile.read(1)[0];
+        while (next > 0) {
+            char nextChar = (char)next;
+            fieldname = fieldname + nextChar;
+            next = (quint8)m_dbFile.read(1)[0];
+        }
+        if (next < 0) {
+            qDebug() << "BIG PROBLEM. Neg number";
+        }
+        m_header.fields[i].setName(fieldname);
+        qDebug() << i << ". " << "fieldName: " << fieldname;
+    }
+}
+
+/*
+* Read in blocks of data from paradox database
+*/
+QList<ParadoxDbBlock> ParadoxReader::readBlocks()
+{
+    long curFileOffest = m_header.headerSize;
+    m_dbFile.seek(curFileOffest);
+    QList<ParadoxDbBlock> blocks;
+    blocks.reserve(m_header.fileBlocks);
+    for (int i = 0; i < m_header.fileBlocks; i++) {
+        int nextBlock = readUShort();
+        int prevBlock = readUShort();
+        int offsetToLastRecord = readShort();
+        curFileOffest += 2 * 3; // 2 byte read 3 times
+        ParadoxDbBlock block(i + 1, nextBlock, prevBlock, offsetToLastRecord, curFileOffest, &m_header);
+        blocks.append(block);
+    }
+    return blocks;
+}
+
+/*
+* Read a short from the paradox database
+* Seeks to the offset entered or reads from the current location if
+* an offset of -1 is passed in
+*/
+qint16 ParadoxReader::readShort(int offset)
+{
+    if (-1 != offset)
+        m_dbFile.seek(offset);
+    QByteArray bytes = m_dbFile.read(2);
+    qint16 value = (qint16)(((qint16)bytes[1]) << 8 | bytes[0]);
+    return value;
+}
+
+/*
+* Read an unsigned byte from the paradox database
+* Seeks to the offset entered or reads from the current location if
+* an offset of -1 is passed in
+*/
+quint8 ParadoxReader::readUByte(int offset)
+{
+    if (-1 != offset)
+        m_dbFile.seek(offset);
+    QByteArray bytes = m_dbFile.read(1);
+    quint8 value = (quint8)bytes[0];
+    return value;
+}
+
+/*
+* Read an integer from the paradox database
+* Seeks to the offset entered or reads from the current location if
+* an offset of -1 is passed in
+*/
+qint32 ParadoxReader::readInt(int offset)
+{
+    if (-1 != offset)
+        m_dbFile.seek(offset);
+    QByteArray bytes = m_dbFile.read(4);
+    qint32 value = (qint32)(((qint32)bytes[3]) << 24 | ((qint32)bytes[2]) << 16 | ((qint32)bytes[1]) << 8 | bytes[0]);
+    return value;
+}
+
+/*
+* Read an unsigned short from the paradox database
+* Seeks to the offset entered or reads from the current location if
+* an offset of -1 is passed in
+*/
+quint16 ParadoxReader::readUShort(int offset)
+{
+    if (-1 != offset)
+        m_dbFile.seek(offset);
+    QByteArray bytes = m_dbFile.read(2);
+    quint16 value = (quint16)(((quint16)bytes[1]) << 8 | bytes[0]);
+    return value;
+}
+
+/*
+* Read a pointer from the paradox database
+* Seeks to the offset entered or reads from the current location if
+* an offset of -1 is passed in
+*/
+qint32 ParadoxReader::readPtr(int offset)
+{
+    return readInt(offset);
+}
+
+/*
+* Adds error message and error occured flag to json object
+* Also closes database connection
+*/
+void ParadoxReader::handleError(QJsonObject* json, const QString& errorMsg)
+{
+    json->insert("ErrorOccured", true);
+    json->insert("ErrorMessage", errorMsg);
+    qDebug() << errorMsg;
+    closeDatabase();
+}
---
+++ src/managers/ParadoxReader.h
@@ -0,0 +1,257 @@
+#pragma once
+
+#include <QString>
+#include <QJsonObject>
+#include <QFile>
+#include <QList>
+
+class ParadoxReader
+{
+public:
+    ParadoxReader(QWidget* parent = Q_NULLPTR, const QString& filePath);
+    ~ParadoxReader();
+
+	QJsonObject Read();
+    void closeDatabase();
+private:
+    void openDatabase(const QString& filePath);
+
+    // Methods for reading in header data
+    void readHeader();
+    bool headerDataValid();
+    void seekToFieldInfoStart();
+    void readHeaderFieldInfo();
+    void skipPastUneededInfo();
+    void readHeaderFieldNames();
+
+    // Read blocks of data
+    QList<ParadoxDbBlock> readBlocks();
+
+    // Helper methods for reading various types of data
+    qint16 readShort(int offset = -1);
+    quint8 readUByte(int offset = -1);
+    qint32 readInt(int offset = -1);
+    quint16 readUShort(int offset = -1);
+    qint32 readPtr(int offset = -1);
+
+    void handleError(QJsonObject* json, const QString& errorMsg);
+
+    QFile m_dbFile;
+    DbHeader m_header;
+};
+
+enum class FieldType
+{
+    Alpha = 0x01,
+    Date = 0x02,
+    ShortInteger = 0x03,
+    LongInteger = 0x04,
+    Currency = 0x05,
+    Number = 0x06,
+    Logical = 0x09,
+    MemoBlob = 0x0C,
+    BLOB = 0x0D,
+    FormatedMemoBlod = 0x0E,
+    OLE = 0x0F,
+    GraphicBlob = 0x10,
+    Time = 0x14,
+    Timestamp = 0x15,
+    AutoInc = 0x16,
+    BCD = 0x17,
+    Bytes = 0x18
+};
+
+class DbHeaderField
+{
+public:
+    DbHeaderField(FieldType type, int size)
+    {
+        m_type = type;
+        m_size = size;
+    }
+    FieldType getType() const { return m_type; }
+    int getSize() const { return m_size; }
+    QString getName() const { return m_name; }
+    void setName(const QString& name) { m_name = name; }
+private:
+    FieldType m_type;
+    int m_size;
+    QString m_name;
+};
+
+class DbHeader
+{
+public:
+    // Directly from bytes
+    qint16 recordSize;
+    qint16 headerSize;
+    quint8 fileType;
+    quint8 maxTableSize;
+    qint32 numRecords;
+    quint16 nextBlock;
+    quint16 fileBlocks;
+    quint16 firstBlock;
+    quint16 lastBlock;
+    quint8 modifiedFlags1;
+    quint8 indexFieldNumber;
+    qint32 primaryIndexWorkspace;
+    qint16 numFields;
+    qint16 primaryKeyFields;
+    qint32 encryption1;
+    quint8 sortOrder;
+    quint8 modifiedFlags2;
+    quint8 changeCount1;
+    quint8 changeCount2;
+    qint32 tableNamePtr;
+    qint32 fieldInfoPtr;
+    quint8 writeProtected;
+    quint8 fileVersionId;
+    quint16 maxBlocks;
+    quint8 auxPassword;
+    qint32 cryptInfoStartPtr;
+    qint32 cryptInfoEndPtr;
+    qint32 autoInc;
+    quint8 indexUpdateRequired;
+    quint8 refIntegrity;
+
+    // Not directly from bytes
+    QList<DbHeaderField> fields;
+};
+
+class ParadoxRecord {
+public:
+    ParadoxRecord(const DbHeader* header) {
+        m_header = header;
+        m_values.reserve(header->numFields);
+        // TODO: do this in a cleaner way
+        for (int i = 0; i < header->numFields; i++) {
+            m_values.append(NULL);
+        }
+    }
+    QVariantList getValues() const { return m_values; }
+
+    void setFieldvalue(int i, const QVariant& value) { m_values[i] = value; }
+    QVariant getFieldvalue(int i) const { return m_values[i]; }
+    QVariant getFieldvalue(const QString& fieldName) const { return m_values[getFieldIndex(fieldName)]; }
+
+private:
+    // Assumes values are located at the same index as the header values
+    const DbHeader* m_header;
+    QVariantList m_values;
+
+    /*
+    * Get the field index
+    */
+    int getFieldIndex(QString fieldName) const
+    {
+        for (int i = 0; i < m_header->fields.count(); i++) {
+            if (m_header->fields[i].getName() == fieldName) {
+                return i;
+            }
+        }
+        // TODO: Decide if an error should be thrown instead of -1
+        return -1;
+    }
+};
+
+class ParadoxDbBlock {
+public:
+    ParadoxDbBlock(const int& blockNumber, const int& nextBlock, const int& prevBlock,
+        const int& offsetToLastRecord, const long& fileOffset, const DbHeader* header) :
+        m_blockNumber(blockNumber),
+        m_nextBlock(nextBlock),
+        m_prevBlock(prevBlock),
+        m_offsetToLastRecord(offsetToLastRecord),
+        m_fileOffset(fileOffset),
+        m_dbHeader(header)
+    {
+    }
+
+    // offsetToLastRecord is set to -header.recordSize when the block is empty.
+    // this method will thus return 0 in this case
+    int getNumRecords() { return (m_offsetToLastRecord / m_dbHeader->recordSize) + 1; }
+
+    QList<ParadoxRecord> readRecords(QFile& dbFile)
+    {
+        int numRecords = getNumRecords();
+        QList<ParadoxRecord> records;
+        records.reserve(numRecords);
+        for (int i = 0; i < numRecords; i++) {
+            records.append(m_dbHeader);
+        }
+        dbFile.seek(m_fileOffset);
+        for (int i = 0; i < numRecords; i++) {
+            records[i] = readRecord(dbFile);
+        }
+        return records;
+    }
+
+private:
+    const int m_blockNumber;
+    const int m_nextBlock;
+    const int m_prevBlock;
+    const int m_offsetToLastRecord;
+    const long m_fileOffset;
+    const DbHeader* m_dbHeader;
+
+    ParadoxRecord readRecord(QFile& dbFile) {
+        ParadoxRecord record(m_dbHeader);
+        for (int i = 0; i < m_dbHeader->numFields; i++) {
+            DbHeaderField field = m_dbHeader->fields[i];
+            FieldType type = field.getType();
+            QByteArray bytes = dbFile.read(field.getSize());
+            QVariant value = getValue(bytes, type);
+            record.setFieldvalue(i, value);
+        }
+        return record;
+    }
+
+    QVariant getValue(QByteArray bytes, FieldType type) {
+        QVariant output;
+
+        switch (type) {
+        case FieldType::Alpha:
+        {
+            QString result;
+            result.reserve(bytes.size());
+            for (int i = 0; i < bytes.size(); i++) {
+                result.append((char)bytes[i]);
+            }
+            output = result;
+            break;
+        }
+        case FieldType::LongInteger:
+        {
+            QByteArray fixed = fixSign(bytes);
+            long value = 0;
+            int leftShift = (bytes.size() - 1) * 8;
+            int mask = 0x000000FF;
+            for (int i = 0; i < bytes.size(); i++) {
+                value |= fixed[i] << leftShift & (mask << leftShift);
+                leftShift = -8;
+            }
+            output = (bytes[0] & 0x80) == 0x80 ? value : (value == 0 ? NULL : -value);
+            break;
+        }
+        // These two are in the data set, but ONYX has them return null
+        // So likely not needed to use the data from these types
+        case FieldType::Logical:
+        case FieldType::MemoBlob:
+            break;
+        default:
+            break;
+        }
+        return output;
+    }
+
+    // Using QByteArray instead of QByteArray& because a copy 
+    // is required and do not want to alias
+    QByteArray fixSign(QByteArray bytes) {
+        if (bytes[0] & 0x80) {
+            //bytes[0] &= 0x7f; This is the java code. not too sure about &=
+            bytes[0] = bytes[0] & 0x7f;
+        }
+        return bytes;
+    }
+};
+
